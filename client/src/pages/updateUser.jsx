e90bf0f12f20695b2b33f2fd4c0d333111f50dbb
Bruna <brunadias_30@hotmail.com>
2023-04-13 17:18:39 -0300

    fix

--- client/src/pages/updateUser.jsx
+++ client/src/pages/updateUser.jsx
@@ -1,37 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
-export default function UpdateUser(props) {
+export default function UpdateUser() {
 
-    const [editValues, setEditValues] = useState({
-        id: props.id,
-        first_name: props.first_name,
-        last_name: props.last_name,
-        email: props.email,
-        phone: props.phone,
-        password: props.password,
-    })
+    const { id } = useParams();
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
 
-    const handleEditUser = (value) => {
-        setEditValues((prevValues) => ({
-            ...prevValues,
-            [value.target.id]: value.target.value,
-        }));
+    async function getData() {
+        await axios.get(`http://localhost:3001/user/${id}`).then((response) => {
+            const userData = response.data[0];
+            setFirstName(userData.first_name)
+            setLastName(userData.last_name)
+            setEmail(userData.email)
+            setPhone(userData.phone)
+            setPassword(userData.password)
+        })
     }
 
+    useEffect(() => { getData() }, [])
+
     function comeback() {
-        window.location('/viewuser');
+        window.location.assign('/viewuser');
     }
 
     function handleUpdateUser() {
         axios.put("http://localhost:3001/update", {
-            id: editValues.id,
-            first_name: editValues.first_name,
-            last_name: editValues.last_name,
-            email: editValues.email,
-            phone: editValues.phone,
-            password: editValues.password,
+            id: id,
+            first_name: firstname,
+            last_name: lastname,
+            email: email,
+            phone: phone,
+            password: password,
         });
         comeback()
     }
@@ -47,15 +52,15 @@ export default function UpdateUser(props) {
                         </div>
                         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
                             <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                id="firstname" type="text" placeholder="First Name*" onChange={handleEditUser} />
+                                id="firstname" type="text" placeholder="First Name*" value={firstname} onChange={(e) => setFirstName(e.target.value)} />
                             <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                id="lastname" type="text" placeholder="Last Name*" onChange={handleEditUser} />
+                                id="lastname" type="text" placeholder="Last Name*" value={lastname} onChange={(e) => setLastName(e.target.value)} />
                             <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                id="email" type="email" placeholder="Email*" onChange={handleEditUser} />
+                                id="email" type="email" placeholder="Email*" value={email} onChange={(e) => setEmail(e.target.value)} />
                             <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                id="phone" type="number" placeholder="Phone*" onChange={handleEditUser} />
+                                id="phone" type="number" placeholder="Phone*" value={phone} onChange={(e) => setPhone(e.target.value)} />
                             <input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 mb-2 rounded-lg focus:outline-none focus:shadow-outline"
-                                id="password" type="password" placeholder="Password*" onChange={handleEditUser} />
+                                id="password" type="password" placeholder="Password*" value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
 
                         <div className="my-2 lg:w-1/4 flex items-center justify-center">
